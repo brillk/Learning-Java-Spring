bbce9cbaeed652a1d277f90fc1bcf2c37d20d1e6
KJH <stonehead334@gmail.com>
2022-07-04 14:49:30 +0900

    API

--- src/main/java/hi/hellospring/controller/HelloController.java
+++ src/main/java/hi/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,4 +21,29 @@ public class HelloController {
         return "hello-template";
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody //response에 직접 넣어준다 스프링은 기본적으로 viewResolver(json)으로 리턴한다
+    public String helloString(@RequestParam("name")String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name) {
+        Hello hello = new Hello(); //객체를 보내면 httpMessageconverter가 기본 등록
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
